3705b381f342a8dd3b549f7f61c1aaffe96a32d4
Yushi_np28@top-academy.ru <Yushi_np28@top-academy.ru>
2025-07-19 00:52:33 +0300

    change main

--- src/main/java/org/top/Main.java
+++ src/main/java/org/top/Main.java
@@ -7,18 +7,23 @@ import org.top.generic.CustomArrayList;
 public class Main {
     public static void testCustomArrayList() {
         CustomArrayList list = new CustomArrayList();
-        System.out.println(list.toString());
+        System.out.println("Добавление элемента по индексу");
         String str = "Hi";
         list.setElementByIndex(0, str);
         System.out.println(list.toString());
+        System.out.println("Добавление элемента в конец");
         Object obj = "Mom";
         list.addElement(obj);
         System.out.println(list.toString());
+        System.out.println("Получение элемента по индексу 1");
         System.out.println(list.getElementByIndex(1));
+        System.out.println("Удаление элемента с конца");
         list.deleteEnd();
         System.out.println(list.toString());
+        System.out.println("Уменьшение до 0");
         list.shrinkOrGrow(0);
         System.out.println(list.toString());
+        System.out.println("Увеличение до 10");
         list.shrinkOrGrow(10);
         System.out.println(list.toString());
     }
